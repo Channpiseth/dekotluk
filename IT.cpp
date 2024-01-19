dc71e2ede184a9a0eecd533911929713840130bd
Channpiseth <channpiseth4riel@gmail.com>
2024-01-19 18:39:58 +0700

    dekluk

---
+++ IT.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"========================== \n";
+    cout<<" Hello My C++ Program \n";
+    cout<<"==========================\n";
+    cout<<"Hello My name is  Piseth \n";
+    cout<<"I COme from beltie school \n";
+    cout<<"i study at BIU \n";
+    cout<<"i want to be a IT manage \n";
+    cout<<"i  don't want to resit exam 20$ \n";
+    cout<<"MY freind name is Heng \n";
+    cout<<"i like to study IT \n";
+
+
+    return 0;
+}
---
+++ kakmes.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"ah jmr \n";
+    return 0;
+}
